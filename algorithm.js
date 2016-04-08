7c4fa1bcf70c29cfa014934ae112043b36bb2845
akim14 <aromie.kim@tufts.edu>
2016-04-08 15:08:24 -0400

    created algorithm.js

---
+++ algorithm.js
@@ -0,0 +1,19 @@
+function algorithm(business) {
+	var base = base_score(business);
+	var plus = bonus(business);
+	var minus = penalty(business);
+
+	return base + plus - minus;
+}
+
+function base_score(business) {
+
+}
+
+function bonus(business) {
+
+}
+
+function penalty(business) {
+
+}
