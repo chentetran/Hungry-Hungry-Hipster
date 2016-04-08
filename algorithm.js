49139562b4ab400e5283feadd67d090687bdf481
akim14 <aromie.kim@tufts.edu>
2016-04-08 15:54:01 -0400

    added json file with 100 biggest restaurant chains

--- algorithm.js
+++ algorithm.js
@@ -1,3 +1,6 @@
+//These commits are coming from the user akim14 because I'm currently
+//borrowing a friend's computer to do this (mine is dead). It's me, Josh!
+
 function algorithm(business) {
 	var base = base_score(business);
 	var plus = bonus(business);
@@ -15,5 +18,6 @@ function bonus(business) {
 }
 
 function penalty(business) {
+	var top100 = JSON.parse('top100restaurants.json');
 
 }
