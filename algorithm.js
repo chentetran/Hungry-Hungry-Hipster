c0c8eed7382f67fe66987a74b89bcf2189fc3ec9
akim14 <aromie.kim@tufts.edu>
2016-04-08 16:28:35 -0400

    added alternate spellings for restaurants on top 100 list

--- algorithm.js
+++ algorithm.js
@@ -1,23 +1,26 @@
 //These commits are coming from the user akim14 because I'm currently
 //borrowing a friend's computer to do this (mine is dead). It's me, Josh!
 
-function algorithm(business) {
-	var base = base_score(business);
-	var plus = bonus(business);
-	var minus = penalty(business);
+function algorithm(businesses) {
+	var base = base_score(businesses);
+	var plus = bonus(businesses);
+	var minus = penalty(businesses);
 
 	return base + plus - minus;
 }
 
-function base_score(business) {
-
-}
-
-function bonus(business) {
-
-}
-
-function penalty(business) {
+function base_score(businesses) {
 	var top100 = JSON.parse('top100restaurants.json');
+	for (var i = 0; i < top100.length; i++) {
+		//if on list, no soup for you
+	}
+
+}
+
+function bonus(businesses) {
+
+}
+
+function penalty(businesses) {
 
 }
